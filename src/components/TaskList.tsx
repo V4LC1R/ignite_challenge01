36bbd7719f6cff31e3756eb8f8f4bc25ea00b08e
V4LC1R <valcir212@gmail.com>
2021-03-14 19:30:03 -0400

    finalizaçao do desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,40 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if(!newTaskTitle)
+      return
+
+    const id = Math.floor(Math.random()*1000);
+
+    const newTask = {
+       id,
+       title:newTaskTitle,
+       isComplete:false
+    }
+
+    setTasks(e=>[...tasks,newTask]);
+    setNewTaskTitle('')
+    
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado IDa
+
+    const reTasks = tasks.map(task=>task.id ===id?{
+      ...task,
+      isComplete:!task.isComplete
+    }:task)
+    
+
+    setTasks(reTasks)
+   
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task=>task.id!==id)
+
+    setTasks(filteredTasks)
   }
 
   return (
